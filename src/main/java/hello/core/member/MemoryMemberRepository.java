bd661b9f7a9db9a2b24b95bc9ac99c70c705f8e1
JoeCP17 <at365@naver.com>
2022-03-03 17:26:29 +0900

    Test 실습

---
+++ src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -0,0 +1,20 @@
+package hello.core.member;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MemoryMemberRepository implements MemberRepository{
+
+    private static Map<Long, Member> store = new HashMap<>();
+    // 이렇게 작성했을때 컨퓨전 해시맵?
+
+    @Override
+   public void save(Member member) {
+
+    }
+
+    @Override
+    public Member findById(Long memberId) {
+        return store.get(memberId);
+    }
+}
