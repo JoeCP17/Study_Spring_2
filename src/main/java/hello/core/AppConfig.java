3a908f57612aeaab0ce81d10b06918e2a22b3595
JoeCP17 <at365@naver.com>
2022-03-13 23:40:29 +0900

    Configuration 바이트코드 조작까지

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -11,21 +11,27 @@ import hello.core.order.orderService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+
+
 @Configuration // 설정 정보에는 다음과 같이 작성
 public class AppConfig {
 
     @Bean // 스프링 컨테이너에 등록이된다.
     public MemberService memberService() {
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
     @Bean
     public MemoryMemberRepository memberRepository() {
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
     @Bean
     public orderService orderService() {
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
+
     @Bean
     public DiscountPolicy discountPolicy () {
         return new RateDiscountPolicy();
